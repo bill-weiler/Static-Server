088c5829cd8a112695fa5945b855309add5a0e64
Student <Student@Mac-mini-2.local>
2016-03-28 17:32:53 -0600

    main completed

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 // Requires \\
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -10,12 +12,20 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); //COMMENT THIS OUT
 
 // Routes \\
+// var fileContents = fs.readFileSync('data.txt', 'utf-8');
+// app.get('/', function(req, res){
+//   res.header('Content-Type', 'text/html');
+//   res.send(fileContents)
+// });
 app.get('/', function(req, res){
-  res.send('Hello')
-});
+  fs.readFile('data.txt', function(err, data){
+      res.header('Content-Type', 'text/html'),
+      res.send(data)
+    });
+})
 
 // Creating Server and Listening for Connections \\
 var port = 3000
